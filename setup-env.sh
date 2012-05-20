89e215566e5f922326a262de40ffbc377c1587ba
Torgny Nyblom <nyblom@kde.org>
2012-05-20 20:49:49 +0200

    Use correct dir

--- setup-env.sh
+++ setup-env.sh
@@ -16,7 +16,14 @@ if [ -z "${WORKSPACE}" ]; then
     FAIL
 fi
 
+if [ -z "${JENKINS_SLAVE_HOME}" ]; then
+    echo "JENKINS_SLAVE_HOME not set!"
+    FAIL
+fi
+
+pushd ${JENKINS_SLAVE_HOME}
 `git fetch origin` && FAIL
 `git checkout ${JENKINS_BRANCH}` && FAIL
 `git merge --ff-only origin/${JENKINS_BRANCH}` && FAIL
 `git log -1 HEAD`
+popd
