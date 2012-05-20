95444f567cc4ec480d0bf5b6a1b8394c7008c6bb
Torgny Nyblom <nyblom@kde.org>
2012-05-20 20:29:36 +0200

    fixes

--- setup-env.sh
+++ setup-env.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -x
 
+function FAIL {
+	# return if sourced and exit if executed
+	[ $0 =~ "bash" ] || return 1
+	exit 1
+}
+
 if [ -z ${JENKINS_BRANCH} ]; then
 	echo "=> JENKINS_BRANCH not set!"
-	export JENKINS_BRANCH="origin/master"
+	export JENKINS_BRANCH="master"
 fi
 
 if [ -z "${WORKSPACE}" ]; then
@@ -10,13 +16,7 @@ if [ -z "${WORKSPACE}" ]; then
     FAIL
 fi
 
-function FAIL {
-	# return if sourced and exit if executed
-	[ $0 ~= "bash" ] || return 1
-	exit 1
-}
-
 `git fetch origin` && FAIL
 `git checkout ${JENKINS_BRANCH}` && FAIL
-`git merge --ff-only ${JENKINS_BRANCH}` && FAIL
+`git merge --ff-only origin/${JENKINS_BRANCH}` && FAIL
 `git log -1 HEAD`
