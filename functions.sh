25693cf42300d94b95d9cc96c255b424adef9c8c
Torgny Nyblom <nyblom@kde.org>
2012-12-09 08:48:01 +0100

    Allow for opensSUSE/Gentoo location for nwalsh stylesheet

--- functions.sh
+++ functions.sh
@@ -371,6 +371,9 @@ function create_packaging_helpers() {
 			echo "=> Generate xml templates..."
 			DOCBOOK_LOCATION=/usr/share/xml/docbook/schema/dtd/4.2/
 			DOCBOOKXSL_LOCATION=/usr/share/xml/docbook/stylesheet/nwalsh/
+			if [[ -d ${DOCBOOK_LOCATION}/current ]]; then
+				DOCBOOKXSL_LOCATION=${DOCBOOK_LOCATION}/current
+			fi
 			sed s#@DOCBOOKXML_CURRENTDTD_DIR@#$DOCBOOK_LOCATION#g borrame/customization/dtd/kdex.dtd.cmake > borrame/customization/dtd/kdex.dtd
 			sed s#@DOCBOOKXSL_DIR@#$DOCBOOKXSL_LOCATION#g borrame/customization/kde-include-common.xsl.cmake > borrame/customization/kde-include-common.xsl
 			sed s#@DOCBOOKXSL_DIR@#$DOCBOOKXSL_LOCATION#g borrame/customization/kde-include-man.xsl.cmake > borrame/customization/kde-include-man.xsl
