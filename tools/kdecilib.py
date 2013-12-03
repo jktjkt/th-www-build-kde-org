c3f52d237c2285469ab3b8a925295dec6a3e900f
Ben Cooksley <bcooksley@kde.org>
2013-12-03 18:45:52 +1300

    Determine the base name properly

--- tools/kdecilib.py
+++ tools/kdecilib.py
@@ -917,7 +917,7 @@ def check_jenkins_environment():
 	# Do we have a job name?
 	if 'JOB_NAME' in os.environ:
 		# Split it out
-		jobMatch = re.match("(?P<project>[^_]+)_?(?P<branch>[^_]+)?_?(?P<base>[^_]+)?", os.environ['JOB_NAME'])
+		jobMatch = re.match("(?P<project>[^_]+)_?(?P<branch>[^_]+)?_?(?P<base>[^_/]+)?", os.environ['JOB_NAME'])
 		# Now transfer in any non-None attributes
 		# If we have the project name, transfer it
 		if jobMatch.group('project') is not None:
