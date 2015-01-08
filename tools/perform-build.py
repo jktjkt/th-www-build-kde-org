d3d09e71f45b5c3b0b25ea7fc996e9eefc38899c
Jan Kundrát <jkt@kde.org>
2015-01-08 18:25:06 +0100

    Add three more build profiles for different Qt versions

--- tools/perform-build.py
+++ tools/perform-build.py
@@ -9,7 +9,7 @@ parser.add_argument('--project', type=str)
 parser.add_argument('--branchGroup', type=str, default='latest-qt4')
 parser.add_argument('--sources', type=str)
 parser.add_argument('--variation', type=str)
-parser.add_argument('--platform', type=str, choices=['linux64-g++', 'darwin-mavericks', 'windows-mingw-w64', 'th-rhel7-gcc'], default='linux64-g++')
+parser.add_argument('--platform', type=str, choices=['linux64-g++', 'darwin-mavericks', 'windows-mingw-w64', 'th-rhel7-gcc', 'th-rhel7-qt52', 'th-rhel7-qt53', 'th-rhel7-qt54'], default='linux64-g++')
 
 # Parse the arguments
 environmentArgs = check_jenkins_environment()
